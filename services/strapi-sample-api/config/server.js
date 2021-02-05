7de559e0b4a719a6c7eb00a4e941e4e322eca380
DUC NGUYEN <vanducld@gmail.com>
2021-02-05 23:21:08 +0700

    Initial

---
+++ services/strapi-sample-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ba813a05e5b8168c7555eeb810bfd65f'),
+    },
+  },
+});
---
+++ services/strapi-sample-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6ddd633b-84d8-4c44-9d7e-11eb7c87bb4a'
+};
